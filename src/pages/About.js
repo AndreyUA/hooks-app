d52ea2d3b03025c0c5c48e904748a37740b875cd
AndreyUA <infernalsteel666@gmail.com>
2020-09-20 17:29:08 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const About = () => {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Страница-заглушка</h1>
+        <p className="lead">
+          Какой-то не нужный текст.
+        </p>
+      </div>
+    </div>
+  );
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Card } from "../components/Card";
+import { Search } from "../components/Search";
+
+export const Home = () => {
+  const cards = new Array(15).fill("").map((_, index) => {
+    return index;
+  });
+
+  console.log(cards);
+
+  return (
+    <>
+      <Search />
+      <div className="row">
+        {cards.map((card) => {
+          return (
+            <div className="col-sm-4 mb-4" key={card}>
+              <Card />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
