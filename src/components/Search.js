68e9bd691c27242a86e3679d3a349784acbf3741
AndreyUA <infernalsteel666@gmail.com>
2020-09-20 20:19:17 +0300

    9-20-2020-usecontext-hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AlertContext } from "../context/alert/alerctContext";
 
 export const Search = () => {
+  const { show } = useContext(AlertContext);
+
+  const onSubmit = (e) => {
+    if (e.key === "Enter") {
+      show("This is alert!");
+    }
+  };
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        onKeyPress={onSubmit}
       />
     </div>
   );
