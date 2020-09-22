068d5aeaf67b6f3a0b9fc082bab20dc67dba9bc1
AndreyUA <infernalsteel666@gmail.com>
2020-09-22 20:44:06 +0300

    9-22-2020-usestate

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,20 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AlertContext } from "../context/alert/alerctContext";
 
 export const Search = () => {
+  const [value, setValue] = useState("");
+
   const { show } = useContext(AlertContext);
 
   const onSubmit = (e) => {
-    if (e.key === "Enter") {
-      show("This is alert!");
+    if (e.key !== "Enter") {
+      return;
+    }
+
+    if (value.trim()) {
+      console.log("make request with: ", value);
+    } else {
+      show("Введите данные пользователя");
     }
   };
 
@@ -17,6 +25,8 @@ export const Search = () => {
         className="form-control"
         placeholder="Введите ник пользователя..."
         onKeyPress={onSubmit}
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </div>
   );
