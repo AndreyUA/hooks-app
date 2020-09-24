d5c91b0171a34ac886bafc3a72c1f4e967331fdd
AndreyUA <infernalsteel666@gmail.com>
2020-09-24 17:01:51 +0300

    9-24-2020-search-complete

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,25 @@
 import React, { useContext, useState } from "react";
 import { AlertContext } from "../context/alert/alerctContext";
+import { GithubContext } from "../context/github/githubContext";
 
 export const Search = () => {
   const [value, setValue] = useState("");
 
-  const { show } = useContext(AlertContext);
+  const alert = useContext(AlertContext);
+  const github = useContext(GithubContext);
 
   const onSubmit = (e) => {
     if (e.key !== "Enter") {
       return;
     }
 
+    github.clearUsers();
+
     if (value.trim()) {
-      console.log("make request with: ", value);
+      alert.hide();
+      github.search(value.trim());
     } else {
-      show("Введите данные пользователя");
+      alert.show("Введите данные пользователя");
     }
   };
 
