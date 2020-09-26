511dbe3c66cd8889f4bcd90672926ae7cf66c0a4
AndreyUA <infernalsteel666@gmail.com>
2020-09-26 14:11:54 +0300

    Updates

---
+++ precache-manifest.6637ac06d863d532bed9b46e5587693b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536d4df7abd51627a1c8ac472b28461c",
+    "url": "/hooks-app/index.html"
+  },
+  {
+    "revision": "dc785822bd0e619e6e10",
+    "url": "/hooks-app/static/css/main.66cfedb4.chunk.css"
+  },
+  {
+    "revision": "2e51a675e60cee86db74",
+    "url": "/hooks-app/static/js/2.f2721122.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hooks-app/static/js/2.f2721122.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc785822bd0e619e6e10",
+    "url": "/hooks-app/static/js/main.1b49a8d8.chunk.js"
+  },
+  {
+    "revision": "34c2925ec36f33c1a2ff",
+    "url": "/hooks-app/static/js/runtime-main.ce9d2450.js"
+  }
+]);
